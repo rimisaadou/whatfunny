7536de57a777c5696dfa02ebc40b044c194baba9
rimi <rimi@brixcrm.nl>
2017-08-03 18:51:16 +0200

    C-2 Added servlet dependency to POM and servletrequest object to call

--- src/main/java/org/rimisaadou/projects/whatfunny/services/WeatherService.java
+++ src/main/java/org/rimisaadou/projects/whatfunny/services/WeatherService.java
@@ -4,18 +4,23 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
+import javax.servlet.http.HttpServletRequest;
 import org.rimisaadou.projects.whatfunny.responses.WeatherResponse;
 
 @Path("/weather")
 public class WeatherService {
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/{location}")
-	public Response getWeather(@PathParam("location") String location) {
+	public Response getWeather(@PathParam("location") String location, 
+			@Context HttpServletRequest requestContext) {
+
+		String ipAddress = requestContext.getRemoteAddr();
+
 		return Response.status(200).entity(new WeatherResponse()).build();
 	}
 
